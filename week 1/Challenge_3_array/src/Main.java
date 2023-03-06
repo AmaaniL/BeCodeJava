1cd9f41c884037435b4410dd0a5d4e8d61d319cc
AmaaniL <malfait99+github@gmail.com>
2023-03-06 21:04:40 +0100

    Add challenge 4

--- week 1/Challenge_3_array/src/Main.java
+++ week 1/Challenge_3_array/src/Main.java
@@ -1,13 +1,14 @@
 import java.util.Arrays;
 import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         int i;
         int[] intArray = new int[10];
-        for (i = 0; i <10; i++) {
+        for (i = 0; i < 10; i++) {
 
             intArray[i] = randomFill();
-            if (intArray[i]<10 ) {
+            if (intArray[i] < 10) {
                 System.out.println(intArray[i]);
             }
 
