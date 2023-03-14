6f4e42cea5bb147e3266383cfe121eefa947f10a
AmaaniL <malfait99+github@gmail.com>
2023-03-14 12:15:24 +0100

    Use stream to read CSV

--- week2/src/main/java/Challenge2_CSVReader.java
+++ week2/src/main/java/Challenge2_CSVReader.java
@@ -1,22 +1,14 @@
 package main.java;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Challenge2_CSVReader {
     public static void main(String[] args) throws IOException {
-        var filePath = "C:\\Users\\Marieke\\becode\\oefeningen\\03.Mountain\\week2\\src\\main\\java\\effects-of-covid-19-on-trade-at-21-July-2021-provisional.csv";
-        var fileEntries = new ArrayList<String>();
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = br.readLine()) != null)
-                fileEntries.add(line);
-        }
-        System.out.println(fileEntries);
-
-        fileEntries.remove(0); // remove the csv header entry
-
+        String filePath = "C:\\Users\\Marieke\\becode\\oefeningen\\03.Mountain\\week2\\src\\main\\java\\effects-of-covid-19-on-trade-at-21-July-2021-provisional.csv";
+        Files.lines(Paths.get(filePath))
+                .skip(0) // ignore the first entry
+                .forEach(System.out::println);
     }
 }
