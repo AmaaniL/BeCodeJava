6632f17398a279e5fea3abd1030dfd3ebf9907cf
AmaaniL <malfait99+github@gmail.com>
2023-03-15 22:13:27 +0100

    Count how many of each direction there are

--- week2/src/main/java/Challenge2_CSVFileStream.java
+++ week2/src/main/java/Challenge2_CSVFileStream.java
@@ -3,20 +3,25 @@ package main.java;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 public class Challenge2_CSVFileStream {
     public static void main(String[] args) throws IOException {
-        Path path = Path.of("C:\\Users\\Marieke\\repos\\03.The-Mountain\\week2\\src\\main\\java\\effects-of-covid-19-on-trade-at-21-July-2021-provisional.csv");
+        Path path = Path.of("C:\\Users\\Marieke\\becode\\oefeningen\\03.Mountain\\week2\\src\\main\\java\\effects-of-covid-19-on-trade-at-21-July-2021-provisional.csv");
 
-        Files.lines(path)
+       Map<String, Long> dataMap = Files.lines(path)
                 .skip(1)
                 .map(Challenge2_CSVFileStream::getData)
                 .filter(challenge2CovidData -> challenge2CovidData.getYear() == 2016) //challenge 3 and 4 filter on year
                 // .filter(challenge2CovidData -> "All".equals(challenge2CovidData.getCountry())) //challenge 3 filter where country is "all"
                 .filter(challenge2CovidData -> "$".equals(challenge2CovidData.getMeasure()))//challenge 4 filter on dollar sign
-                .collect(Collectors.toList())
-                .forEach(System.out::println);
+                .collect(Collectors.groupingBy(Challenge2_CovidData::getDirection, counting()));//counts how many of each direction there are.
+        System.out.println(dataMap);
+                //.forEach(System.out::println);
 
     }
 
