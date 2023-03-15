185a6cc077e5f00af6da7c9494ab06f15b61c229
Marieke Malfait <malfait99+becode@gmail.com>
2023-03-15 20:55:08 +0100

    Add exercise 4 filtering datastreams

--- week2/src/main/java/Challenge2_CSVFileStream.java
+++ week2/src/main/java/Challenge2_CSVFileStream.java
@@ -3,8 +3,7 @@ package main.java;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.List;
+import java.util.stream.Collectors;
 
 public class Challenge2_CSVFileStream {
     public static void main(String[] args) throws IOException {
@@ -13,17 +12,20 @@ public class Challenge2_CSVFileStream {
         Files.lines(path)
                 .skip(1)
                 .map(Challenge2_CSVFileStream::getData)
-                .filter(challenge2CovidData -> challenge2CovidData.getYear() == 2016) //challenge 3 filter on year
-                .filter(challenge2CovidData -> "All".equals(challenge2CovidData.getCountry())) //challenge 3 filter where country is "all"
+                .filter(challenge2CovidData -> challenge2CovidData.getYear() == 2016) //challenge 3 and 4 filter on year
+                // .filter(challenge2CovidData -> "All".equals(challenge2CovidData.getCountry())) //challenge 3 filter where country is "all"
+                .filter(challenge2CovidData -> "$".equals(challenge2CovidData.getMeasure()))//challenge 4 filter on dollar sign
+                .collect(Collectors.toList())
                 .forEach(System.out::println);
 
     }
 
     private static Challenge2_CovidData getData(String line) {
 
-         String[] fields = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+        String[] fields = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
 
         return new Challenge2_CovidData(fields[0], Integer.parseInt(fields[1]), fields[2], fields[3], fields[4], fields[5], fields[6], fields[7], Long.parseLong(fields[8]), Long.parseLong(fields[9])); //lines to object
+
 
     }
 }
