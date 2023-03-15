9b7c69b6fcc2c583b969578ca6a705b4964c536c
AmaaniL <malfait99+github@gmail.com>
2023-03-15 22:15:27 +0100

    Group by country and put it in a Map called dataMap

--- week2/src/main/java/Challenge2_CSVFileStream.java
+++ week2/src/main/java/Challenge2_CSVFileStream.java
@@ -7,19 +7,17 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.*;
-
 public class Challenge2_CSVFileStream {
     public static void main(String[] args) throws IOException {
         Path path = Path.of("C:\\Users\\Marieke\\becode\\oefeningen\\03.Mountain\\week2\\src\\main\\java\\effects-of-covid-19-on-trade-at-21-July-2021-provisional.csv");
 
-       Map<String, Long> dataMap = Files.lines(path)
+       Map<String, List<Challenge2_CovidData>> dataMap = Files.lines(path)
                 .skip(1)
                 .map(Challenge2_CSVFileStream::getData)
                 .filter(challenge2CovidData -> challenge2CovidData.getYear() == 2016) //challenge 3 and 4 filter on year
                 // .filter(challenge2CovidData -> "All".equals(challenge2CovidData.getCountry())) //challenge 3 filter where country is "all"
                 .filter(challenge2CovidData -> "$".equals(challenge2CovidData.getMeasure()))//challenge 4 filter on dollar sign
-                .collect(Collectors.groupingBy(Challenge2_CovidData::getDirection, counting()));//counts how many of each direction there are.
+                .collect(Collectors.groupingBy(Challenge2_CovidData::getCountry));
         System.out.println(dataMap);
                 //.forEach(System.out::println);
 
